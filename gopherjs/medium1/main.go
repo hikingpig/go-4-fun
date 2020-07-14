df79584477d19a0a4f85402e4c2c61c98d89e0e7
phuoc <phuocdongxoai@gmail.com>
2020-07-14 20:10:53 +0700

    gopherjs more

--- gopherjs/medium1/main.go
+++ gopherjs/medium1/main.go
@@ -8,12 +8,50 @@ import (
 
 //go:generate gopherjs build --minify
 
+type UserInfo struct {
+	*js.Object
+	Name    string `js:"name"`
+	Address string `js:"address"`
+	City    string `js:"city"`
+	State   string `js:"state"`
+	Hash    []byte `js:"hash"`
+}
+
 func main() {
 	js.Module.Get("exports").Set("hashit", hashit)
+	js.Module.Get("exports").Set("hashobj", hashobj)
+	js.Module.Get("exports").Set("hashobj2", hashobj2)
 }
 
 func hashit(s string) []byte {
 	h := sha256.New()
 	h.Write([]byte(s))
 	return h.Sum(nil)
+}
+
+func hashobj(obj map[string]interface{}) []byte {
+	h := sha256.New()
+	for _, k := range []string{"name", "address", "city"} {
+		if s, ok := obj[k]; ok {
+			switch st := s.(type) {
+			case string:
+				h.Write([]byte(st))
+			case []byte:
+				h.Write(st)
+			default:
+			}
+		}
+	}
+	return h.Sum(nil)
+}
+
+func hashobj2(obj *js.Object) *js.Object {
+	ui := &UserInfo{Object: obj}
+
+	h := sha256.New()
+	h.Write([]byte(ui.Name))
+	h.Write([]byte(ui.Address))
+	h.Write([]byte(ui.City))
+	ui.Hash = h.Sum(nil)
+	return ui.Object
 }
--- gopherjs/medium1/test.js
+++ gopherjs/medium1/test.js
@@ -1,5 +1,21 @@
-simple = require("./medium1.js")
+m = require("./medium1.js")
 
-h = simple.hashit("This is a test")
+h = m.hashit("This is a test")
 
-console.log(h.length)
+console.log("Test hashit result: ", h.length)
+
+h = m.hashobj({
+    name: "Kent",
+    address: "Somewhere in Space and Time",
+    city: "Specificity",
+    state: "CT"
+})
+console.log("Test hashobj result: ", h.slice(0, 4))
+
+h = m.hashobj2({
+  name: "Kent",
+  address: "Somewhere in Space and Time",
+  city: "Specificity",
+  state: "CT"
+})
+console.log("Test hashobj2 result: ", h.hash.slice(0, 4))
